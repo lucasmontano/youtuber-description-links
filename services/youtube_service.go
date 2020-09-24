b21c185d49856865a00d27c0a5fec7de24963aae
ArthurFleischman <cabralfle@gmail.com>
2020-09-24 14:11:39 -0300

    refactoring variable name videoId tp videoID

--- services/youtube_service.go
+++ services/youtube_service.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"lucasmontano.com/yt-links/models"
 	"net/http"
 	"net/url"
 	"os"
 	"os/user"
 	"path/filepath"
+
+	"lucasmontano.com/yt-links/models"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
@@ -87,10 +88,10 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 func saveToken(file string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", file)
 	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	defer f.Close()
 	if err != nil {
 		log.Fatalf("Unable to cache oauth token: %v", err)
 	}
-	defer f.Close()
 	json.NewEncoder(f).Encode(token)
 }
 
@@ -124,9 +125,9 @@ func GetVideosService() models.PlaylistItemsResponse {
 	}
 
 	for _, channel := range response.Items {
-		playlistId := channel.ContentDetails.RelatedPlaylists.Uploads
+		playlistID := channel.ContentDetails.RelatedPlaylists.Uploads
 		// Print the playlist ID for the list of uploaded videos.
-		fmt.Printf("Videos in list %s\r\n", playlistId)
+		fmt.Printf("Videos in list %s\r\n", playlistID)
 
 		nextPageToken := ""
 		for {
@@ -134,7 +135,7 @@ func GetVideosService() models.PlaylistItemsResponse {
 			// list of uploaded videos. Each request retrieves 50
 			// videos until all videos have been retrieved.
 			playlistCall := service.PlaylistItems.List([]string{"snippet"}).
-				PlaylistId(playlistId).
+				PlaylistId(playlistID).
 				MaxResults(50).
 				PageToken(nextPageToken)
 
